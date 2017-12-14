51e1cb064627c4a635fe70f0c6b1efdadf014ae5
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-12-13 20:09:22 -0500

    Documented CardPileItem and Deck

--- CardPileItem.h
+++ CardPileItem.h
@@ -8,15 +8,51 @@
 class CardPileItem : public GameItem
 {
 public:
+  /**
+   * @brief PADDING the padding to put between piles of cards
+   */
   const static size_t PADDING = 20;
 
+  /**
+   * @brief CardPileItem constructs a new CardPileItem to be drawn for the specified window.
+   * @param window the window to draw the pile on.
+   * @param pile the pile to be represented by this CardPileItem.
+   */
   CardPileItem(MainWindow* window, CardPile& pile);
 
+  /**
+   * @brief paint paints the CardPile with the specified painter.
+   * @param painter the painter to be used to paint.
+   * @param option
+   * @param widget
+   */
   void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+  /**
+   * @brief boundingRect returns the bounding rectangle for this CardPileItem.
+   * @return the bounding rectangle for this CardPileItem.
+   */
   QRectF boundingRect() const;
+
+  /**
+   * @brief mouseClickToCardIndex converts the location of the specified mouse click to a card index in the CardPile.
+   * @param event the MouseEvent
+   * @return the index of the card that was clicked.
+   */
   size_t mouseClickToCardIndex(QGraphicsSceneMouseEvent* event);
 
+  /**
+   * @brief draw handles drawing the specified CardPile to the specified painter.
+   * @param painter the painter to use to draw.
+   * @param cardPile the CardPile to be drawn.
+   */
   static void draw(QPainter* painter, CardPile& cardPile);
+
+  /**
+   * @brief bounds calculates and returns the bounds for the specified CardPile.
+   * @param cardPile the CardPile to calculate the bounding box for.
+   * @return the bounding box for the specified CardPile.
+   */
   static QRectF bounds(const CardPile& cardPile);
 
 protected:
--- Deck.h
+++ Deck.h
@@ -7,11 +7,33 @@
 class Deck
 {
 public:
+  /**
+   * @brief Deck creates a new Deck.
+   */
   Deck();
 
+  /**
+   * @brief draw draws a card from the Deck.
+   * @return the Card that was drawn from the Deck.
+   */
   Card draw();
+
+  /**
+   * @brief shuffle shuffles the Deck.
+   */
   void shuffle();
+
+  /**
+   * @brief add adds the specified Card back in the deck.
+   * @param card the Card to add.
+   */
   void add(Card card);
+
+  /**
+   * @brief isEmpty determines whether or not the Deck is empty.
+   * @return true if the Deck is empty.
+   * @return false if the Deck isn't empty.
+   */
   bool isEmpty() const;
 
 private:
