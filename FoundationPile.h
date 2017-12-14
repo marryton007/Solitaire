d3b355f3ebeb4e11bae4c4ed9416e51c0fe8209b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-12-13 20:22:22 -0500

    Documented FoundationPile

--- FoundationPile.h
+++ FoundationPile.h
@@ -8,8 +8,16 @@
 class FoundationPile : public CardPile
 {
 public:
+  /**
+   * @brief FoundationPile creates a new FoundationPile.
+   */
   FoundationPile();
 
+  /**
+   * @brief accepts determines whether or not the specified Card is accepted by this FoundationPile.
+   * @return true if the Card is accepted.
+   * @return false if the Card isn't accepted.
+   */
   bool accepts(const Card &) override;
 };
 
--- FoundationPileItem.h
+++ FoundationPileItem.h
@@ -7,7 +7,12 @@
 class FoundationPileItem : public GameItem
 {
 public:
-  FoundationPileItem(MainWindow* window, FoundationPile& m_pile);
+  /**
+   * @brief FoundationPileItem creates a new FoundationPileItem to handle drawing a FoundationPile to the Window.
+   * @param window the window to draw the FoundationPile on.
+   * @param pile the FoundationPile to represent.
+   */
+  FoundationPileItem(MainWindow* window, FoundationPile& pile);
 
   QRectF boundingRect() const;
   void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
